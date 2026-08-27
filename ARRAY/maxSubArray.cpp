486052ce3b728849ee62f4272989928b27dc4a03
Shamik Das <dasshamik6@gmail.com>
2026-08-27 19:37:42 +0530

    Refactor code structure for improved readability and maintainability

---
+++ ARRAY/maxSubArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> arr = {14,25,36,1,28,67,35};
+    int size = arr.size();
+    int maxSum = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        int currentSum = 0;
+        for (int j = 0; j < size; j++)
+        {
+            currentSum += arr[j];
+            maxSum = max(maxSum,currentSum);
+        }
+        
+    }
+    
+    cout << "Maximum subarray sum = " << maxSum;
+    return 0;
+}
